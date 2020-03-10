c440cfa987a42bb03aea3828ddc7224e9ea8aa6e
Arif Ali <arif@Arifs-MacBook-Pro.local>
2020-03-10 17:49:41 +0000

    added changes to readme and file search tags

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# instructions
+# Directorylist
 - cmake is used for compilation.
 - open terminal and type `cd directorylist`, then  `cmake -S. -Bbuild` to build make files from `CMakeLists.txt`
 - in the same directory type `make -C build && build/main` to build and run executable.
--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include "directorylist.h"
 
 int main(){
-    std::string dir_path = "testdata/";
-    std::vector<std::string> matchDir = {"TESTS"};
-    std::vector<std::string> excludeDir = {"SRC", "DATA"};
-    std::vector<std::string> matchExt = {"XYZ"};
+    const std::string dir_path = "path_to_dirs/";
+    const std::vector<std::string> matchDir = {"data"};
+    const std::vector<std::string> excludeDir = {"dir1", "dir2"};
+    const std::vector<std::string> matchExt = {"PDF"};
 
     Query query;
     query.search(dir_path, matchDir, excludeDir, matchExt);
